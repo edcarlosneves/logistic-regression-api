8a0a0fb927448c2fc400cf80a98e9d5ce175535d
Edcarlos Neves <edcarlosp@gmail.com>
2022-05-30 23:10:13 -0300

    changed router and urls

--- project/apps/classifiers/urls.py
+++ project/apps/classifiers/urls.py
@@ -7,18 +7,12 @@ from project.apps.classifiers.api import (
     DatafileHeaderViewSet,
 )
 
-router_data_file = routers.DefaultRouter()
-router_data_file.register(r"", DataFileViewSet, basename="DataFile")
-urlpatterns = [path("data-csv/", include(router_data_file.urls))]
+router = routers.DefaultRouter()
+router.register("data-csv", DataFileViewSet, basename="DataFile")
+router.register("data-csv-header", DatafileHeaderViewSet, basename="DatafileHeader")
+router.register("classifier", ClassifierViewSet, basename="Classifier")
 
-router_data_file_header = routers.DefaultRouter()
-router_data_file_header.register(r"", DatafileHeaderViewSet, basename="DatafileHeader")
-urlpatterns += [path("data-csv-header/", include(router_data_file_header.urls))]
-
-router_classifier = routers.DefaultRouter()
-router_classifier.register(r"", ClassifierViewSet, basename="Classifier")
-urlpatterns += [path("classifier/", include(router_classifier.urls))]
-
-urlpatterns += [
+urlpatterns = [
+    path("", include(router.urls)),
     path("predict/", PredcitResultsApiView.as_view()),
 ]
